da98eb72a6f5b987c53bf1632246a09a28170a71
Adrien Bouyssou <adrien.bouyssou@gmail.com>
2022-08-01 11:21:49 +0200

    REF Move State in its own component

--- src/board/Board.jsx
+++ src/board/Board.jsx
@@ -1,4 +1,5 @@
 import "./Board.css";
+import State from "./State";
 
 export default function Board({ states }) {
   return (
@@ -6,60 +7,7 @@ export default function Board({ states }) {
       <h1>Task board</h1>
       <div className="board">
         {states.map((state) => (
-          <div
-            style={{
-              backgroundColor: "#dddddd",
-              borderRadius: "10px",
-              boxShadow: "1px 1px #aaaaaa",
-              margin: 0,
-              padding: "10px"
-            }}
-          >
-            <h2>{state.name}</h2>
-            <div
-              style={{
-                margin: 0,
-                padding: 0
-              }}
-            >
-              {state.tasks.map((task) => (
-                <div
-                  style={{
-                    backgroundColor: "#bbbbbb",
-                    border: "1px solid #aaaaaa",
-                    boxShadow: "1px 1px #999999",
-                    borderRadius: "10px",
-                    marginBottom: "10px",
-                    display: "flex",
-                    flexDirection: "column"
-                  }}
-                >
-                  <h3 style={{ marginBottom: "10px" }}>{task.name}</h3>
-                  <div
-                    className="flexCentered"
-                    style={{
-                      flexWrap: "wrap",
-                      gap: "5px",
-                      marginBottom: "10px"
-                    }}
-                  >
-                    {task.tags.map((tag) => (
-                      <div
-                        style={{
-                          padding: "5px",
-                          backgroundColor: "#dddddd",
-                          boxShadow: "1px 1px #999999",
-                          borderRadius: "5px"
-                        }}
-                      >
-                        {tag}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+          <State state={state} />
         ))}
       </div>
     </div>
